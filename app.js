bdb257b53e1897eafa2da6772703ec1ed2cd85f0
Albert Palau <anorkbcn@gmail.com>
2017-08-01 12:50:13 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,31 @@
+const Influx = require('influx')
 const express = require('express')
 var http = require('http');
 const app = express()
 
 var url= 'http://api.coindesk.com/v1/bpi/currentprice/EUR.json';
 var bitcoins = 0.03013318;
+var expended = 52.0;
+
+const influx = new Influx.InfluxDB({
+  host: '127.0.0.1',
+  database: 'cryptocurrencies_db',
+  schema: [
+    {
+      measurement: 'cryptocurrencies_rates',
+      fields: {
+        exchange: Influx.FieldType.STRING,
+        rate_float_usd: Influx.FieldType.FLOAT,
+        rate_float_eur: Influx.FieldType.FLOAT,
+        amount_owned: Influx.FieldType.FLOAT,
+	total_expended: Influx.FieldType.FLOAT
+      },
+      tags: [
+        'currency'
+      ]
+    }
+  ]
+})
 
 app.get('/', function (req, result) {
 	http.get(url, function(res){
@@ -14,12 +36,23 @@ app.get('/', function (req, result) {
 		
 		res.on('end', function(){
 			var cdResponse = JSON.parse(body);
-			rate = cdResponse.bpi['EUR'].rate_float;
-			console.log("Got a response: ", rate);
-			console.log("rate: ", rate);
+			rate_eur = cdResponse.bpi['EUR'].rate_float;
+			rate_usd = cdResponse.bpi['USD'].rate_float;
+			console.log("Got a response: ", rate_eur);
+			console.log("rate: ", rate_eur);
 		        console.log("bitcoins: ", bitcoins);
-		        eur_amount = bitcoins * rate;
-                        body = "Bitcoins: " + bitcoins + "<br> Rate: " + rate + "<br> Total: " + eur_amount;
+		        eur_amount = bitcoins * rate_eur;
+                        body = "Bitcoins: " + bitcoins + "<br> Rate: " + rate_eur + "<br> Total: " + eur_amount;
+
+			influx.writePoints([
+			      {
+				        measurement: 'cryptocurrencies_rates',
+				        tags: { currency: 'BTC' },
+				        fields: { exchange: 'coinbase', rate_float_usd: rate_usd, rate_float_eur: rate_eur, amount_owned: bitcoins, total_expended: expended },
+			      }
+			 ]).catch(err => {
+			      console.error(`Error saving data to InfluxDB! ${err.stack}`)
+			 })
 			result.send(body);
 		});
 	}).on('error', function(e){
